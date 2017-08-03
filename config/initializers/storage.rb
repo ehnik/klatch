1ad7046a8a0577e830771a74cf525896cf94038f
Kay Ehni <kay.ehnigmail.com>
2017-08-03 15:22:37 -0400

    encrypted aws credentials

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,11 +1,15 @@
 CarrierWave.configure do |config|
+
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV["aws_access_id"],
-      aws_secret_access_key: ENV["aws_access_secret_key"],
+      aws_access_key_id:     ENV["S3_ACCESS_KEY"],
+      aws_secret_access_key: ENV["S3_SECRET_KEY"],
       region: 'us-east-2'
   }
   config.storage = :fog
-  config.fog_directory  = 'klatch-images'
+  config.fog_directory  = ENV['S3_Bucket']
   config.fog_public     = false
 end
