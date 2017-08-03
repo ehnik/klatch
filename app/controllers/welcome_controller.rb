177d60b50f6bfa4efcf4a46df9a1f3c2f6371dab
Kay Ehni <kay.ehnigmail.com>
2017-08-03 19:35:10 -0400

    added welcome controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+class WelcomeController < ApplicationController
+
+  def index
+    render 'users/registrations/new'
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   devise_for :users, :controllers => {:registrations => "users/registrations",
     :sessions => "users/sessions", :passwords => "users/passwords"}
   devise_scope :user do
