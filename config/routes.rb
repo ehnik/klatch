809be6065d55b0cf1abd4ec69aaf9ba6d898b091
Kay Ehni <kay.ehnigmail.com>
2017-08-03 19:48:59 -0400

    experiment with renamed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   devise_for :users, :controllers => {:registrations => "users/registrations",
     :sessions => "users/sessions", :passwords => "users/passwords"}
   devise_scope :user do
